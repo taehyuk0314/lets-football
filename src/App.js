01093e009d4cef194a46f208bf91f8319a90d8e3
taehyuk0314 <taehyek0314@gmail.com>
2021-08-23 21:46:22 +0900

    project install

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+
+
+
+class App extends React.Component {
+  render() {
+    return <h1>Hello, {this.props.name}</h1>;
+  }
+}
+export default App;
