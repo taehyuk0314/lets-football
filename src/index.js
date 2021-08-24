74cc7e250a8aba7a0e1d269ec239d623aba909a7
DESKTOP-JGMJGR9\taehy <taehyek0314@naver.com>
2021-08-24 12:54:55 +0900

    서버 연결 테스트

--- src/index.js
+++ src/index.js
@@ -2,7 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './views/main/main';
 import reportWebVitals from './reportWebVitals';
+import axios from 'axios'
+require("./assets/css/style.css");
 
+
+
+axios.defaults.baseURL = process.env.REACT_APP_BASE_URL_API;
+axios.defaults.withCredentials = true;
 ReactDOM.render(
   <React.StrictMode>
     <App />
--- src/views/main/main.js
+++ src/views/main/main.js
@@ -1,9 +1,22 @@
 import React, { Component } from 'react';
+import axios from 'axios'
 
 class main extends Component {
+  clickButton(){
+    console.log(axios.defaults.baseURL);
+    // axios.get(`/main`)
+    // .then((r) => {
+    //   alert(r);
+    // });    
+  }
   render() {
     return (
-      <div>리액트는 처음이지?</div>
+      <div id="container">이거한번 해보셈
+        <div className="contents-filter">
+          <button onClick={this.clickButton}>버튼</button>
+        </div>
+      </div>
+      
     );
   }
 }
